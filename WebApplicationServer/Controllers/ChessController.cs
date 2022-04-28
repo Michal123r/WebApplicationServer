806982e8559a4596cb1367c339f769414b8d730d
michal <michal12r@gmail.com>
2022-04-29 00:10:15 +0300

    data

---
+++ WebApplicationServer/Controllers/ChessController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplicationServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChessController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ChessController> _logger;
+
+        public ChessController(ILogger<ChessController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+        //[EnableCors]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> steps;
+            steps = DAL.DAL.GetData();
+            return steps.ToArray();
+
+        }
+    }
+}
---
+++ WebApplicationServer/DAL/DAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WebApplicationServer.DAL
+{
+    public class DAL
+    {
+        internal static List<string> GetData()
+        {
+            List<string> allSteps = new List<string>();
+            //var response = JsonConvert.DeserializeObject<object>(@"DATA\\steps.json");
+            //string json = File.ReadAllText(@"steps.json");
+            //var playerList = JsonConvert.DeserializeObject<List<string>>(json);
+            string filePath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory)) + @"\DATA\steps.json";
+            string json = File.ReadAllText(filePath);
+            var steps = JsonConvert.DeserializeObject<List<string>>(json);
+            foreach (var item in steps)
+            {
+                allSteps.Add(item);
+            }
+            return allSteps;
+        }
+    }
+}
